b5c4d1e0e3bb4b361f92847b93de7cd0e4a16dfd
yifeic <yifei.chen@outlook.com>
2015-06-15 09:38:07 -0400

    don't stop ranging, only stop location updates

--- Pod/Classes/CYFBeaconManager.m
+++ Pod/Classes/CYFBeaconManager.m
@@ -68,7 +68,7 @@
         
         [self.regionExitSignal subscribeNext:^(CLBeaconRegion *region) {
             
-            [self.locationManager stopRangingBeaconsInRegion:region];
+            // [self.locationManager stopRangingBeaconsInRegion:region];
         }];
         
         
@@ -129,7 +129,7 @@
     for (CLBeaconRegion *region in self.regions) {
         [self.locationManager startMonitoringForRegion:region];
 //        [self.locationManager requestStateForRegion:region];
-        // [self.locationManager startRangingBeaconsInRegion:region];
+        [self.locationManager startRangingBeaconsInRegion:region];
     }
     
     //startUpdatingLocation keeps the app alive in background
